a3132865fb00e831eecb65a486bf2f68dadc02c2
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2019-07-31 12:11:53 +1000

    Echo library added

--- apiservice/apisvcmain.go
+++ apiservice/apisvcmain.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/labstack/echo/v4"
+import "github.com/labstack/echo/v4/middleware"
+
 func main() {
-	
+	apiServer := echo.New()
+	apiServer.Use(middleware.Logger())
+	apiServer.Use(middleware.Recover())
+	if apiServer == nil {
+		apiServer.Logger.Debug("Error in starting server")
+	}
 }
