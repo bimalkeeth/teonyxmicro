4488e5f82f9ec17c18a9edf41cda6e4e209ffda0
Bimal <bimalkeeth@gmail.com>
2019-08-11 21:55:45 +1000

    Implementing entities

--- mastersvc/entities/table_contacttype.go
+++ mastersvc/entities/table_contacttype.go
@@ -1,14 +1,4 @@
 package entities
 
-import "github.com/jinzhu/gorm"
-
-type TableAddress struct {
-	gorm.Model
-	Address       string `gorm:"column:address;not_null"`
-	Street        string `gorm:"column:street:not_null"`
-	Suberb        string `gorm:"column:suberb"`
-	StateId       uint   `gorm:"column:stateid"`
-	CountryId     uint   `gorm:"column:countryid"`
-	AddressTypeId uint   `gorm:"column:addresstypeid;not_null"`
-	Location      string `gorm:"column:location;not_null"`
+type TableContactType struct {
 }
