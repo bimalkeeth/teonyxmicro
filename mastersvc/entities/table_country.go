c0272fa7a48a4ed2d40e9efd18d921433c535ae6
Bimal <bimalkeeth@gmail.com>
2019-08-11 22:13:00 +1000

    Implementing entities

--- mastersvc/entities/table_country.go
+++ mastersvc/entities/table_country.go
@@ -1,10 +1,25 @@
 package entities
 
 import "github.com/jinzhu/gorm"
+import "errors"
 
 type TableCountry struct {
 	gorm.Model
 	CountryName string `gorm:"column:countryname;not_null"`
 	RegionId    uint   `gorm:"column:regionid;not_null"`
 	Region      *TableRegion
+}
+
+func (t TableCountry) TableName() string {
+	return "table_contacttype"
+}
+
+func (t TableCountry) Validate(db *gorm.DB) {
+
+	if len(t.CountryName) == 0 {
+		_ = db.AddError(errors.New("country name should contain value"))
+	}
+	if t.RegionId == 0 {
+		_ = db.AddError(errors.New("region should contain value"))
+	}
 }
