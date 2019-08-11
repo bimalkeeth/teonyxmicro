5f82683d058d7c7b6c6c42d9ab614681b7a84c80
Bimal <bimalkeeth@gmail.com>
2019-08-11 22:10:42 +1000

    Implementing entities

--- mastersvc/entities/table_country.go
+++ mastersvc/entities/table_country.go
@@ -1,4 +1,10 @@
 package entities
 
+import "github.com/jinzhu/gorm"
+
 type TableCountry struct {
+	gorm.Model
+	CountryName string `gorm:"column:countryname;not_null"`
+	RegionId    uint   `gorm:"column:regionid;not_null"`
+	Region      *TableRegion
 }
