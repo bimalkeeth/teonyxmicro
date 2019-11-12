f149e4b53b1bb912a1758e47e20b73be40451e71
Bimal Kaluarachchi <bimalkeeth@gmail.com>
2019-11-13 01:47:37 +1100

    Vehicle Table

--- mastersvc/manager/operator/manager_operators_contact.go
+++ mastersvc/manager/operator/manager_operators_contact.go
@@ -1,6 +1,7 @@
 package operator
 
 import (
+	bu "teonyxmicro/mastersvc/bucontracts"
 	bs "teonyxmicro/mastersvc/business"
 )
 
@@ -31,5 +32,29 @@ func (o *OprManager) UpdateOperatorContact(id uint, contactId uint, operatorId u
 		return res, err
 	}
 	OpFac.Conn.Commit()
+	return res, err
+}
+
+//----------------------------------------------------
+//Delete operation contact
+//----------------------------------------------------
+func (o *OprManager) DeleteOperatorContact(id uint) (bool, error) {
+	op := OpFac.New(bs.COperationContact).(bs.OperatorContact)
+	OpFac.Conn.Begin()
+	res, err := op.DeleteOperatorContact(id)
+	if err != nil {
+		OpFac.Conn.Rollback()
+		return res, err
+	}
+	OpFac.Conn.Commit()
+	return res, err
+}
+
+//----------------------------------------------------
+//Get all operation contact
+//----------------------------------------------------
+func (o *OprManager) GetAllContactsByOperator(operatorId uint) ([]bu.OperatorContactsBO, error) {
+	op := OpFac.New(bs.COperationContact).(bs.OperatorContact)
+	res, err := op.GetAllContactsByOperator(operatorId)
 	return res, err
 }
